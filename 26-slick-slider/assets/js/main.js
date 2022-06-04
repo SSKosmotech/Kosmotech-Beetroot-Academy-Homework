40e38f3d1645d182d023c3c8bb9265f9593420b1
Serhii Simonov <ss@kosmotech.com.ua>
2022-06-04 18:04:02 +0300

    Bottom slider  26-slick-slider26-slick-slider

--- 26-slick-slider/assets/js/main.js
+++ 26-slick-slider/assets/js/main.js
@@ -3,14 +3,19 @@ $(function(){
         infinite: true,
         dots: true,
         lazyLoad: 'progressive',
+        arrows: false,
+        autoplay: true,
+        autoplaySpeed: 3000,
     });
 
 
     $('.product_slider').slick({
-        // slidesToShow: 5,
+        slidesToShow: 5,
         // centerMode: true,
-        // infinite: true,
-        // slidesToScroll: 5,
+        infinite: true,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 3000,
 
         // slidesPerRow: 5,
         // centerPadding: '10px'
@@ -18,16 +23,26 @@ $(function(){
         // customPaging: '10px',
         // lazyLoad: 'progressive',
 
-        slidesToShow: 5,
-        slidesToScroll: 1,
-        centerMode: true,
-        arrows: true,
-        dots: false,
-        speed: 300,
-        centerPadding: '20px',
-        infinite: true,
-        autoplaySpeed: 5000,
-        autoplay: true
+        // slidesToShow: 5,
+        // slidesToScroll: 1,
+        // centerMode: true,
+        // arrows: true,
+        // dots: false,
+        // speed: 300,
+        // centerPadding: '20px',
+        // infinite: true,
+        // autoplaySpeed: 5000,
     });
+
+    $('.partners_slider').slick({
+        slidesToShow: 9,
+        // centerMode: true,
+        infinite: true,
+        slidesToScroll: 1,
+        // slidesPerRow: 5,
+        centerPadding: '150px'
+        // autoplay: true,
+        // autoplaySpeed: 5000,
+    })
 
 })
