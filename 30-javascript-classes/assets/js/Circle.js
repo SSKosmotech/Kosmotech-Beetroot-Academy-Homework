be2109fffbe558096c7a46bf46ef7fbc6ff9da62
Serhii Simonov <ss@kosmotech.com.ua>
2022-06-21 00:59:37 +0300

    30-javascript-classes 1 & 3

--- 30-javascript-classes/assets/js/Circle.js
+++ 30-javascript-classes/assets/js/Circle.js
@@ -2,30 +2,51 @@ class Circle {
     constructor (radius){
         this.radius = radius;
     }
-    show(){
-        let target = document.querySelector('#circle');
-        target.insertAdjacentHTML('beforeend', `
+    showCircle(){
+        // let target = document.querySelector('#circle');
+        // target.insertAdjacentHTML('beforeend', `
+        // <div class="circle" style="width: ${this.radius*2}px;height:${this.radius*2}px;background-color:red;position: absolute; border-radius: 50%"></div>
+        // `);
+
+        document.getElementById('circle').innerHTML = 
+        ` 
             <div class="circle" style="width: ${this.radius*2}px;height:${this.radius*2}px;background-color:red;position: absolute; border-radius: 50%"></div>
-        `);
+        `;
     }
-    get myRadius() {
+    get getRadius() {
         return this.radius;
     } 
-    set myRadius(myRadius) {
+    set setRadius(myRadius) {
         this.radius = myRadius;
     }
-    get myDiametr() {
+    showGetRadius() {
+        document.getElementById('radius').innerText = this.getRadius;
+    }
+    get getDiametr() {
         return this.radius*2;
-    } 
+    }
+    showDiametr() {
+        document.getElementById('diametr').innerText = this.getDiametr;
+    }
     squareCircle() {
-        let square = 0;
-        square = Math.PI * Math.pow(this.radius,2);
-        console.log(`Square Circle = ${square}`);
+        // let square = 0;
+        // return square = (Math.PI * Math.pow(this.radius,2)).toFixed(2);
+        return (Math.PI * Math.pow(this.radius,2)).toFixed(2);
+        // console.log(`Square Circle = ${square}`);
+    }
+    showSquareCircle() {
+        // console.log(`See Square: ${s}`);
+        document.getElementById('square_circle').innerText = this.squareCircle();
     }
     lineWidthCircle() {
-        let lineWidth = 0;
-        lineWidth = 2 * Math.PI * this.radius;
-        console.log(`Line Width Circle = ${lineWidth}`);
+        // let lineWidth = 0;
+        // lineWidth = (2 * Math.PI * this.radius).toFixed(2);
+        // console.log(`Line Width Circle = ${lineWidth}`);
+        
+        return (2 * Math.PI * this.radius).toFixed(2);
     }
-    
+    showLineWidthCircle() {
+        // console.log(`See Line: ${l}`);
+        document.getElementById('line_width_circle').innerText = this.lineWidthCircle();
+    }
 }
--- 30-javascript-classes/assets/js/main.js
+++ 30-javascript-classes/assets/js/main.js
@@ -1,17 +1,77 @@
-const circ = new Circle (50);
-circ.show();
 
-const circ2 = new Circle (60);
-console.log(circ2.myRadius);
+// <<==========================1 Task==============================>>
 
-circ2.myRadius = '80';
-console.log(circ2.myRadius);
+// const circ = new Circle (8);
+// circ.show();
 
-console.log(circ2.myDiametr);
+const circ = new Circle (60);
+console.log(`Get Radius before set: ${circ.getRadius}`);
 
-circ2.show();
+circ.setRadius = '8';
+
+console.log(`Get Radius after Set: ${circ.getRadius}`);
+console.log(`Get Diametr: ${circ.getDiametr}`);
+
+circ.squareCircle();
+circ.lineWidthCircle();
 
 
-circ2.squareCircle();
+circ.showGetRadius();
+circ.showDiametr();
+circ.showSquareCircle();
+circ.showLineWidthCircle();
 
-circ2.lineWidthCircle();
+circ.showCircle();
+
+
+
+
+
+
+// // HW 2
+
+// const kolo = new Circle();
+
+// const diam = kolo.diametr; // так викликається getter
+
+// kolo.showDiametr('d');
+
+
+// //
+// const kolo2 = {
+//     radius: 10,
+//     getDiametr(){
+//         return this.radius * 2;
+//     }
+// }
+
+// kolo2.getDiametr()//20
+// kolo2.radius = 20;
+// // kolo2.
+
+
+
+
+// <<==========================3 Task==============================>>
+
+const list = [
+    new Employee({
+        name: 'Bob', 
+        position: 'Manager', 
+        age: 30
+    }),
+    new Employee({
+        name: 'John', 
+        position: 'High Manager', 
+        age: 35
+    }),
+    new Employee({
+        name: 'Ann', 
+        position: 'Admin', 
+        age: 28
+    })
+];
+
+const myTable = new EmpTable(list);
+
+myTable.viewHtml('emp_tbody');
