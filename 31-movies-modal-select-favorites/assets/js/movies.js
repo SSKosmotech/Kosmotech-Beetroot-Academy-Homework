4251e0451bcf2e94ceb091c653a3619154a86a84
Serhii Simonov <ss@kosmotech.com.ua>
2022-07-03 00:13:05 +0300

    31-movies-modal-select-favorites UP

--- 31-movies-modal-select-favorites/assets/js/movies.js
+++ 31-movies-modal-select-favorites/assets/js/movies.js
@@ -6,9 +6,9 @@ const movieItem = {
             // this.$parent.getMovieInfo(id)
             this.$emit("getMovie", id)
         },
-        setFavorites(id, title) {
+        setFavorites(id) {
             // this.$parent.setFavorites(id, title)
-            this.$emit("setFavorit", id, title)
+            this.$emit("setFavorit", id)
         }
     },
     
@@ -51,7 +51,7 @@ const App = {
                 .then(function () {
                     // always executed
                 });
-                this.myFavorite = JSON.parse(localStorage.getItem('myFavorite'));
+                // this.myFavorite = JSON.parse(localStorage.getItem('myFavorite'));
             }
         },
         showMovieInfo(){
@@ -76,15 +76,14 @@ const App = {
                 // always executed
             });
         },
-        setFavorites(id, title){
-            this.myFavorite = JSON.parse(localStorage.getItem('myFavorite')) || [];
+        setFavorites(id){
+            // this.myFavorite = JSON.parse(localStorage.getItem('myFavorite')) || [];
             // this.myFavorite = id
-            console.log(id, title)
-            if(this.myFavorite.find(el => el.id===id) === undefined){
-                this.myFavorite.push({
-                    id: id,
-                    title: title
-                });
+            console.log(id)
+            const movie = this.movieList.find(el => el.imdbID===id)
+            const delFavoriteIndex = this.myFavorite.findIndex(el => el.imdbID===id);
+            if(delFavoriteIndex === -1){
+                this.myFavorite.push(movie);
                 // console.log(this.myFavorite);
                 // localStorage.setItem('myFavorite', JSON.stringify(this.myFavorite));
             //     localStorage.removeItem('myFavorite', JSON.stringify(this.myFavorite));
@@ -92,7 +91,6 @@ const App = {
             else{
                 // localStorage.removeItem(this.myFavorite);
                 // localStorage.removeItem('myFavorite', JSON.stringify(this.myFavorite));
-                const delFavoriteIndex = this.myFavorite.findIndex(el => el.id===id);
                 console.log(`delFavoriteIndex= ${delFavoriteIndex}`);
 
                 this.myFavorite.splice(delFavoriteIndex, 1);
@@ -101,22 +99,22 @@ const App = {
             localStorage.setItem('myFavorite', JSON.stringify(this.myFavorite));
             console.log(this.myFavorite);
         },
-        created() {
-            this.myFavorite = JSON.parse(localStorage.getItem('myFavorite'));
-        },
+    
         showFavorites(){
             this.showFavoritesModal = true
-            this.myFavorite = JSON.parse(localStorage.getItem('myFavorite'));
+            // this.myFavorite = JSON.parse(localStorage.getItem('myFavorite'));
             console.log(`Check: ${this.myFavorite}`);
-        },
+        }
+    },
+    computed: {
         // 02 Work
         movieListWithFavorite(){
             const arr = []
             
             this.movieList.forEach(el => {
                 const findFav = this.myFavorite.find(item => {
-                    // return el.imdbID === item.imdbID
-                    return el.imdbID === item.id
+                    return el.imdbID === item.imdbID
+                    // return el.imdbID === item.id
                 })
 
                 el.inFav = findFav !== undefined ? true : false
@@ -132,6 +130,10 @@ const App = {
             console.log(arr);
             return arr
         }
+    },
+    created() {
+        this.myFavorite = JSON.parse(localStorage.getItem('myFavorite')) || [];
+        // this.myFavorite = JSON.parse(localStorage.getItem('myFavorite'));
     }
 }
 
